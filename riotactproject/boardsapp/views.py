276c32278022e0f4eb8c0089127b3eb8a7d1271f
Kyle Morton <kylemortondevelopment@gmail.com>
2020-02-27 23:44:44 -0600

    working w/ forms api

--- riotactproject/boardsapp/views.py
+++ riotactproject/boardsapp/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import NewTopicForm
 from .models import Board, Topic, Post
 
 
@@ -14,25 +16,20 @@ def board_topics(request, pk): # pk from URL
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-
+    user = User.objects.first()  # TODO: get the currently logged in user
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-
-        user = User.objects.first()  # TODO: get the currently logged in user
-
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            creator = user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-
-        return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
-
-    return render(request, 'new_topic.html', {'board': board})
+        form = NewTopicForm(request.POST) # send post data to form model constructor
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
